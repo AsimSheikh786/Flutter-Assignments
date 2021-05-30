e7560b6c4fb39005c1d9229995b52658439364ca
AsimSheikh786 <64584660+AsimSheikh786@users.noreply.github.com>
2021-05-30 13:52:11 +0500

    Flutter Assignment 2 complete

--- main.dart
+++ main.dart
@@ -9,8 +9,60 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Text("Shaikh Muhammad Asim Qadri"),
+        appBar: AppBar(
+          title: Text("Login Page"),
+          backgroundColor: Colors.deepPurpleAccent,
+          shadowColor: Colors.blueGrey,
+          elevation: 5.0,
+          actions: [
+            IconButton(
+              icon: Icon(Icons.person_pin),
+              iconSize: 30,
+            ),
+            IconButton(
+              icon: Icon(Icons.phone_enabled_rounded),
+            ),
+          ],
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 300,
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: "Enter your email",
+                    hintText: "Email",
+                  ),
+                ),
+                ),
+                SizedBox(height: 50,),
+              Container(
+                width: 300,
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: "Enter your password",
+                    hintText: "Password",
+                  ),
+                ),
+                ),
+                SizedBox(height: 50,),
+                ElevatedButton(
+                  onPressed: (){},
+                  child: Text("Login Now"),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.deepPurple,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        ),
       ),
     );
   }
